d55a9e69e312d433dd9a0657f40124dde46611d8
Jeffrey-Rivera <jeffreyrivera711@gmail.com>
2024-10-06 00:35:04 -0600

    home and navbar buttons

---
+++ src/app/pages/doctor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Doctors = () => {
+    return (
+        <div>
+            <h1>Doctors Page</h1>
+            <p>Information about doctors...</p>
+        </div>
+    );
+};
+
+export default Doctors;
